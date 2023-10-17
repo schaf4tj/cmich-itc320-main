41d538eb00f63519158e5b27dbb72a38aad5aad9
schaf4tj <144141265+schaf4tj@users.noreply.github.com>
2023-10-16 20:05:25 -0400

    Update calculate_tax.js

--- labs/lab_05/calculate_tax.js
+++ labs/lab_05/calculate_tax.js
@@ -5,4 +5,42 @@ document.addEventListener("DOMContentLoaded", () => {
 	// add event handlers
 	$("#calculate").addEventListener("click", processEntry);
 
+	function processEntry() {
+		const entryElement = $("#entry");
+		const resultElement = $("#result");
+		const entryValue = parseFloat(entryElement.value);
+	
+		entryElement.focus();
+	
+		if (isNaN(entryValue) || entryValue <= 0) {
+			resultElement.value = "Invalid input. Enter a # < 0.";
+		} else {
+			const taxAmount = calculateTax(entryValue);
+			resultElement.value = "Tax Amount: $" + taxAmount.toFixed(2);
+		}
+	}
+
+	function calculateTax(income) {
+		income = Math.floor(income);
+
+		const brackets = [9875, 40125, 85525, 163300, 207350, 518400];
+		const rates = [0.10, 0.12, 0.22, 0.24, 0.32, 0.35, 0.37];
+
+		let taxAmount = 0;
+		let remainingIncome = income;
+
+		for (let i = 0; i < brackets.length; i++) {
+			if(remainingIncome <= 0) {
+				break;
+			}
+
+			const taxableIncome = Math.min(remainingIncome, brackets[i]);
+			taxAmount += taxableIncome * rates[i];
+			remainingIncome -= taxableIncome;
+		}
+
+		taxAmount = Math.round(taxAmount * 100) / 100;
+
+		return taxAmount;
+	}
 });

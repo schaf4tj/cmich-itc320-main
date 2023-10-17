81ce692fdf72a86c13c626a1e50d151f48aa05f3
schaf4tj <144141265+schaf4tj@users.noreply.github.com>
2023-10-16 20:07:29 -0400

    Update calculate_tax.js

--- labs/lab_04/calculate_tax.js
+++ labs/lab_04/calculate_tax.js
@@ -12,52 +12,51 @@ document.addEventListener("DOMContentLoaded", () => {
 	$("#itemPrice").addEventListener("click", clearTextBox);
 	$("#taxRate").addEventListener("click", clearTextBox);
 
-});
-
-function processEntry() {
-	// get user inputs
-	const itemPrice = parseFloat($("#itemPrice").value);
-	const taxRate = parseFloat($("#taxRate").value);
-	const salesTaxResult = $("#salesTaxResult");
-	const totalResult = $("#TotalResult");
-
-	// data validation
-	if(isNaN(itemPrice) || itemPrice <= 0 || itemPrice >= 10000) {
-		alert("Invalid input. Enter a valid # less than 10,000.");
+	function processEntry() {
+		// get user inputs
+		const itemPrice = parseFloat($("#itemPrice").value);
+		const taxRate = parseFloat($("#taxRate").value);
+		const salesTaxResult = $("#salesTaxResult");
+		const totalResult = $("#TotalResult");
+	
+		// data validation
+		if(isNaN(itemPrice) || itemPrice <= 0 || itemPrice >= 10000) {
+			alert("Invalid input. Enter a valid # less than 10,000.");
+			// set focus
+			$("#itemPrice").focus();
+			return;
+		}
+		 if(isNaN(taxRate) || taxRate <= 0 || taxRate >= 12) {
+			alert("Invalid input. Enter a valid # less than 12.");
+			// set focus
+			$("#taxRate").focus();
+			return;
+		}
+	
+		// calculate
+		const salesTax = itemPrice * (taxRate / 100);
+		const total = itemPrice + salesTax;
+	
+		// display results
+		salesTaxResult.value = salesTax.toFixed(2);
+		totalResult.value = total.toFixed(2);
+	
+		// set focus
+		clearFields();
+	}
+	
+	function clearFields() {
+		// clear boxes
+		clearTextBox($("#itemPrice"));
+		clearTextBox($("#taxRate"));
+		$("#salesTaxResult").value = "";
+		$("#totalResult").value = "";
+	
 		// set focus
 		$("#itemPrice").focus();
-		return;
 	}
-	 if(isNaN(taxRate) || taxRate <= 0 || taxRate >= 12) {
-		alert("Invalid input. Enter a valid # less than 12.");
-		// set focus
-		$("#taxRate").focus();
-		return;
+	
+	function clearTextBox(textBox) {
+		textBox.value = "";
 	}
-
-	// calculate
-	const salesTax = itemPrice * (taxRate / 100);
-	const total = itemPrice + salesTax;
-
-	// display results
-	salesTaxResult.value = salesTax.toFixed(2);
-	totalResult.value = total.toFixed(2);
-
-	// set focus
-	clearFields();
-}
-
-function clearFields() {
-	// clear boxes
-	clearTextBox($("#itemPrice"));
-	clearTextBox($("#taxRate"));
-	$("#salesTaxResult").value = "";
-	$("#totalResult").value = "";
-
-	// set focus
-	$("#itemPrice").focus();
-}
-
-function clearTextBox(textBox) {
-	textBox.value = "";
-}
+});

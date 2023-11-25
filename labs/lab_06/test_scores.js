c64a54be6d353ab00b8555e95593c24e530a4043
schaf4tj <144141265+schaf4tj@users.noreply.github.com>
2023-11-25 18:55:18 -0500

    lab work

--- labs/lab_06/test_scores.js
+++ labs/lab_06/test_scores.js
@@ -10,3 +10,51 @@ document.addEventListener("DOMContentLoaded", () => {
 	$("#display_results").addEventListener("click", displayResults);
 	$("#display_scores").addEventListener("click", displayScores);
 });
+
+function addScore() {
+	//random #
+	//const randomScore = Math.floor(Math.random() * 100) + 1;
+	//scores.push(randomScore);
+	//alert(`Score ${randomScore} added.`);
+	const scores = [];
+
+	let addscore = 0;
+	do {
+		score = parseInt (
+			prompt("Enter a test score, or enter -1 to end scores", -1));
+		
+		if (scores >= 0 && score <= 100) {
+			scores[scores.length] = score;
+		}
+		else if (score != -1) {
+			alert ("score must be a valid # from 0 throguh 100");
+		}
+	}
+	while(scores != -1);
+
+	if (scores.length > 0) {
+		let total = 0;
+		for(let i in scores) {
+			total = total + scores[i];
+			document.write('<p>Score ${parseInt(i) + 1}: ${scores[i]}</p>');
+		}
+
+	}
+  }
+
+  function displayResults() {
+	const totalScore = scores.reduce((acc, score) => acc + score, 0);
+	const averageScore = totalScore / (scores.length - 1); // Exclude the -1 from the count
+
+	const resultsContainer = $("#resultsContainer");
+	resultsContainer.innerHTML = `<p>Total Score: ${totalScore}</p><p>Average Score: ${averageScore.toFixed(2)}</p>`;
+  }
+
+  function displayScores() {
+	const scoresContainer = $("#scoresContainer");
+	scoresContainer.innerHTML = "<p>Scores:</p>";
+
+	for (let i = 0; i < scores.length - 1; i++) {
+	  scoresContainer.innerHTML += `<p>Score ${i + 1}: ${scores[i]}</p>`;
+	}
+  }

f90a829c678aa7823eb81af1801707bffd68a0c4
schaf4tj <144141265+schaf4tj@users.noreply.github.com>
2023-11-25 21:02:37 -0500

    lab 9 work

--- labs/lab_09/reservation.js
+++ labs/lab_09/reservation.js
@@ -1,53 +1,95 @@
 "use strict";
 
-$(document).ready( () => {
-	const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
-		
-	// Move focus to the "Arrival date" text box
-		const arrivalDateInput = $("#arrivalDate");
-		arrivalDateInput.focus();
+$(document).ready(() => {
+    const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
 
-	// Event handler for form submission
-	$("#reservationForm").submit(function (event) {
-		console.log("Form submitted");
-
-		// Validate user entries
-		const nameInput = $("#name");
+    const validateForm = () => {
+        const arrivalDateInput = $("#arrival_date");
+        const nightsInput = $("#nights");
+        const nameInput = $("#name");
+        const phoneInput = $("#phone");
 		const emailInput = $("#email");
-		const nightsInput = $("#nights");
 
-		// Trim and put back entries into the controls
-		nameInput.val(nameInput.val().trim());
-		emailInput.val(emailInput.val().trim());
-		nightsInput.val(nightsInput.val().trim());
 
-		// Check if any of the entries are invalid
-        if (!isValidInput(nameInput) || !isValidInput(emailInput) || !isValidNumericInput(nightsInput)) {
-            // Display an alert or handle invalid entries as needed
-			console.log("Invalid entries. Form will not submit.");
-            alert("Please fill in all fields with valid information.");
+        // Reset error messages
+        arrivalDateInput.next().text("");
+        nightsInput.next().text("");
+        nameInput.next().text("");
+		phoneInput.next().text("");
+        emailInput.next().text("");
 
-            // Prevent the form from submitting
-            event.preventDefault();
-            return false; // Make sure to return false to cancel the submission
+        let isValid = true;
+
+        // Validate arrival date
+        if (arrivalDateInput.val().trim() === "") {
+            arrivalDateInput.next().text("Arrival date is required.");
+            isValid = false;
         }
-		console.log("Form submission allowed.");
-		return true; // Allow the form to submit
+
+        // Validate nights
+        const nightsValue = nightsInput.val().trim();
+        if (nightsValue === "" || isNaN(Number(nightsValue))) {
+            nightsInput.next().text("Please enter a valid number of nights.");
+            isValid = false;
+        }
+
+        // Validate name
+        if (nameInput.val().trim() === "") {
+            nameInput.next().text("Name is required.");
+            isValid = false;
+        }
+
+        // Validate email
+        const emailValue = emailInput.val().trim();
+        if (emailValue === "" || !emailPattern.test(emailValue)) {
+            emailInput.next().text("Please enter a valid email address.");
+            isValid = false;
+        }
+
+		// Validate phone number (assuming a simple format like 999-999-9999)
+        const phoneValue = phoneInput.val().trim();
+        if (phoneValue === "" || !/^\d{3}-\d{3}-\d{4}$/.test(phoneValue)) {
+            phoneInput.next().text("Please enter a valid phone number (e.g., 999-999-9999).");
+            isValid = false;
+        }
+
+        // If all entries are valid, submit the form
+        if (isValid) {
+            $("form").submit();
+        }
+    };
+
+    const resetForm = () => {
+        $("form")[0].reset();
+        $("#arrival_date").next().text("");
+        $("#nights").next().text("");
+        $("#name").next().text("");
+        $("#email").next().text("");
+		$("#phone").next().text("");
+        $("#arrival_date").focus();
+    };
+
+    $("#display_results").click(() => {
+        // Logic to display results (not provided in the prompt)
     });
 
-    // Function to check if a value is non-empty
-    function isValidInput(input) {
-        return input.val().trim() !== "";
-    }
+    $("#display_scores").click(() => {
+        // Logic to display scores (not provided in the prompt)
+    });
 
-    // Function to check if a value is numeric
-    function isValidNumericInput(input) {
-        const value = input.val().trim();
-        return isNumeric(value);
-    }
+    $("#add").click(() => {
+        // Logic to add scores (not provided in the prompt)
+    });
 
-    // Function to check if a value is numeric
-    function isNumeric(value) {
-        return !isNaN(value) && isFinite(value);
-    }
-}); // end ready
+    $("#reservation_form").submit((event) => {
+		console.log("Form submission triggered");
+        event.preventDefault(); // Prevent the default form submission
+        validateForm(); // Validate the form entries
+    });
+
+    $("#reset_form").click(() => {
+        resetForm(); // Reset the form
+    });
+
+    $("#arrival_date").focus(); // Set initial focus on arrival date
+});

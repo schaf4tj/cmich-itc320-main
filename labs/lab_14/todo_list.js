9d6d69ac9bd5770d789029f9562f6a2960ce4809
schaf4tj <144141265+schaf4tj@users.noreply.github.com>
2023-11-26 00:00:32 -0500

    finishing labs

--- labs/lab_14/todo_list.js
+++ labs/lab_14/todo_list.js
@@ -1,3 +1,57 @@
 "use strict";
 
 const domain = "https://jsonplaceholder.typicode.com";
+
+const displayUsers = async () => {
+    try {
+      const response = await fetch(`${domain}/users`);
+      const users = await response.json();
+  
+      const usersSelect = document.getElementById("users");
+      users.forEach(user => {
+        const option = document.createElement("option");
+        option.value = user.id;
+        option.text = user.name;
+        usersSelect.appendChild(option);
+      });
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    displayUsers();
+  
+    document.getElementById("view_list").addEventListener("click", async () => {
+      const userId = document.getElementById("users").value;
+  
+      try {
+        const response = await fetch(`${domain}/todos?userId=${userId}`);
+        const todos = await response.json();
+  
+        const listContainer = document.getElementById("list");
+        listContainer.innerHTML = ""; // Clear previous content
+  
+        if (todos.length > 0) {
+          const table = document.createElement("table");
+          const headerRow = table.insertRow();
+          headerRow.insertCell().textContent = "ID";
+          headerRow.insertCell().textContent = "Title";
+          headerRow.insertCell().textContent = "Completed";
+  
+          todos.forEach(todo => {
+            const row = table.insertRow();
+            row.insertCell().textContent = todo.id;
+            row.insertCell().textContent = todo.title;
+            row.insertCell().textContent = todo.completed ? "Yes" : "No";
+          });
+  
+          listContainer.appendChild(table);
+        } else {
+          listContainer.textContent = "No todos found for the selected user.";
+        }
+      } catch (error) {
+        console.error("Error fetching todos:", error);
+      }
+    });
+  });
--- labs/lab_15/rover.js
+++ labs/lab_15/rover.js
@@ -7,7 +7,20 @@ const request = "?api_key=DEMO_KEY&page=1";
 const roverData = new Map();
 
 // asynchronous getJson() function that makes API requests goes here
-
+const getJson = async (url) => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error fetching JSON:", error);
+      throw error; // Re-throw the error to handle it at the caller level if needed
+    }
+  };
+  
 const getSelectedDate = () => {
     const year = $("#year").val();
     const month = $("#month").val();
